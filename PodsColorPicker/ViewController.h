47296963df6d33fd6f00fa4d692b082bbaf57ea6
KPicart <kpicart@stepwisedesigns.com>
2015-09-15 05:25:06 -0400

    not working in B/W

---
+++ PodsColorPicker/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  PodsColorPicker
+//
+//  Created by MacMan on 9/15/15.
+//  Copyright (c) 2015 MacManApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FCColorPickerViewController.h"
+
+
+
+@interface ViewController : UIViewController <FCColorPickerViewControllerDelegate>
+- (IBAction)colorME:(id)sender;
+
+
+@end
+
